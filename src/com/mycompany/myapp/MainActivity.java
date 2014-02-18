a206035df5e2bc396766480b769c830e299c193e
kor-ka <badgrib@gmail.com>
2014-02-18 17:25:23 +0400

    init

--- src/com/mycompany/myapp/MainActivity.java
+++ src/com/mycompany/myapp/MainActivity.java
@@ -1,17 +1,40 @@
 package com.mycompany.myapp;
 
 import android.app.*;
+import android.content.*;
 import android.os.*;
-import android.view.*;
-import android.widget.*;
+import it.gmariotti.cardslib.library.internal.*;
+import it.gmariotti.cardslib.library.view.*;
+import java.util.*;
 
 public class MainActivity extends Activity
 {
+	Context ctx;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		ctx = this;
+		ArrayList<Card> cards = new ArrayList<Card>();
+
+		//Create a Card
+		Card card = new Card(ctx);
+
+		//Create a CardHeader
+		CardHeader header = new CardHeader(ctx);
+		header.setTitle("hi!");
+		//Add Header to card
+		card.addCardHeader(header);
+
+		cards.add(card);
+		
+		CardArrayAdapter mCardArrayAdapter = new CardArrayAdapter(this,cards);
+
+        CardListView listView = (CardListView) this.findViewById(R.id.fullCardsList);
+        if (listView!=null){
+            listView.setAdapter(mCardArrayAdapter);
+        }	
     }
 }
