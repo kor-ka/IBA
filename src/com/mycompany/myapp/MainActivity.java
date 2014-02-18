aa682bb39860a3d2d400f18b5f65edc95e044dfe
kor-ka <badgrib@gmail.com>
2014-02-18 17:16:48 +0300

    Json start

--- src/com/mycompany/myapp/MainActivity.java
+++ src/com/mycompany/myapp/MainActivity.java
@@ -6,10 +6,15 @@ import android.os.*;
 import it.gmariotti.cardslib.library.internal.*;
 import it.gmariotti.cardslib.library.view.*;
 import java.util.*;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.HashSet;
+import java.util.HashMap;
 
 public class MainActivity extends Activity
 {
 	Context ctx;
+	String baseJsonString;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -31,6 +36,26 @@ public class MainActivity extends Activity
 		cards.add(card);
 		
 		CardArrayAdapter mCardArrayAdapter = new CardArrayAdapter(this,cards);
+		
+		baseJsonString = getString(R.string.base);
+		
+		 try {
+		 	JSONObject baseJson = new JSONObject(baseJsonString);
+		 	JSONArray jArray = baseJson.getJSONArray("base");
+		 	
+		 	for(int i = 0; i < jArray.length(); i++)){
+		 		try {
+		 			JSONObject oneObject = jArray.getJSONObject(i);	
+		 		} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+		 	}//for end
+		 } catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
         CardListView listView = (CardListView) this.findViewById(R.id.fullCardsList);
         if (listView!=null){
